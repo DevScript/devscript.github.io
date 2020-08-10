cc20c984055a8b465d255d6a4222afcb5ddce354
Ankit <ankitchaudhari1598@gmail.com>
2020-08-10 18:17:58 +0530

    :hammer: removed unnecessary code

--- assets/js/main.js
+++ assets/js/main.js
@@ -220,17 +220,3 @@
   });
 
 })(jQuery);
-
-
-$('.bxslider').bxSlider({
-  autoHover: true,
-  auto: true,
-  slideWidth: 250,
-  minSlides: 2,
-  maxSlides: 6,
-  controls: true,
-  pager: true,
-  speed: 500,
-  captions: true,
-  slideMargin: 5,
-});
