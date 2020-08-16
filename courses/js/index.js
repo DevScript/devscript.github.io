3048ef9801704c0a1d01be52bd51af3b59dca9d7
Ankit <ankitchaudhari1598@gmail.com>
2020-08-17 01:16:32 +0530

    :boom: Added Courses Page

---
+++ courses/js/index.js
@@ -0,0 +1,66 @@
+var myObject = new Vue({
+  el: '#app',
+  data: {
+    cards: [
+      { 
+        title: 'Computer Systems and Organisation - I', 
+        topic: 'Basic computer organisation, Types of software, Memory Units, Boolean logic' 
+      },
+      { 
+        title: 'Computer Systems and Organisation - II', 
+        topic: 'Number System, Encoding Schemes, Concept of Compiler and Interpreter, Operating System' 
+      },
+      { 
+        title: 'Computational Thinking and Programming', 
+        topic: 'Introduction to Problem solving, flowchart, pseudo-code' 
+      },
+      { 
+        title: 'Introduction to Python', 
+        topic: 'Basics of Python programming, data-types, process of writing a program, Interactive & Script mode' 
+      },
+      { 
+        title: 'Working with Python - I', 
+        topic: 'Python Character Set, Token & Identifiers, Keywords, Literals, Delimiters, Operators' 
+      },
+      { 
+        title: 'Working with Python - II', 
+        topic: 'Comments, Introduce the notion of a variable, Knowledge of data types and operators, Operators & type' 
+      },
+      { 
+        title: 'Working with Python - III', 
+        topic: 'Execution of a program, errors- syntax error, run-time error and logical error' 
+      },
+      { 
+        title: 'Iterative Statements', 
+        topic: 'for, while, nested for loop, Notion of iterative computation' 
+      },
+      { 
+        title: 'Conditional Statements', 
+        topic: 'conditional if, elif, else, nested if, flow control, Ternary Operator' 
+      },
+      { 
+        title: 'Strings', 
+        topic: 'Traversal, concatenation, repetition, membership, functions/methods' 
+      },
+      { 
+        title: 'Lists', 
+        topic: 'Definition, Creation of a list, Traversal of a list, concatenation, repetition, membership, functions/methods' 
+      },
+      { 
+        title: 'Tuples', 
+        topic: 'Definition, Creation of a Tuple, Traversal of a tuple, concatenation, repetition, membership, functions/methods' 
+      },
+      { 
+        title: 'Dictionary', 
+        topic: 'Definition, Creation, Accessing elements of a dictionary, add an item, modify an item in a dictionary, Traversal, functions/methods' 
+      },
+      { 
+        title: 'Sets', 
+        topic: 'Comments, Introduce the notion of a variable, Knowledge of data types and operators, Operators & type' 
+      },
+      { 
+        title: 'Introduction to Python modules', 
+        topic: 'math module, random module, statistics module' 
+      },]
+  }
+})
