30cb38a8c0b277c730a45be61762bb88bf5eaa2f
Ankit <ankitchaudhari1598@gmail.com>
2020-08-23 01:12:30 +0530

    :zap: improved verification page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,29 @@
 // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyC4BLOTJtE_MnQ2uc4EbydZD9IjxOPPoFU",
-      authDomain: "website-connection-test.firebaseapp.com",
-      databaseURL: "https://website-connection-test.firebaseio.com",
-      projectId: "website-connection-test",
-      storageBucket: "website-connection-test.appspot.com",
-      messagingSenderId: "901705844467",
-      appId: "1:901705844467:web:3ed567aaaca69667e05986",
-      measurementId: "G-MN9D3SDE4X"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+	apiKey: "AIzaSyBjEuC81_bZmJyl5enVPhrDBANlLyBLfts",
+	authDomain: "devscript-certifications.firebaseapp.com",
+	databaseURL: "https://devscript-certifications.firebaseio.com",
+	projectId: "devscript-certifications",
+	storageBucket: "devscript-certifications.appspot.com",
+	messagingSenderId: "1007881633914",
+	appId: "1:1007881633914:web:9086014de5b5531010bbc3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-    const db = firebase.firestore();
+const db = firebase.firestore().collection('certifications');
 
 const certs = document.querySelector('.verify-card');
 const form = document.querySelector('#cert-form');
 const content = jQuery('.verify-content');
+const code = document.querySelector('.verify-code');
+const msg = document.querySelector('.verify-box-message');
 
 function renderCert(doc){
 
-	let html = `<table class='verify-body table table-hover' data-id = ${String(doc.id)}>`;
-	var titles = ['Name', 'Email ID', 'Certificate ID', 'Description'] //, 'Created At'];
-	var values = ['name', 'emailid', 'cert_id', 'desc'] //, 'createdAt']
+	let html = '<table class="verify-body table table-hover" data-id = ' + doc.id + '>';
+	var titles = ['Name', 'Email ID', 'Certificate ID', 'Description'];
+	var values = ['name', 'email', 'certificate_id', 'description'];
 
 	var i;
 	for(i=0;i<titles.length;i++){
@@ -34,20 +35,24 @@ function renderCert(doc){
 	}
     html += '</table>'
 
+	form.remove();
 	content.append(html);
 }
-
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	db.collection('certificates').where('cert_id', '==', form.certid.value).get().then((snapshot) => {
+	db.where('certificate_id', '==', form.certid.value).get().then((snapshot) => {
 		snapshot.docs.forEach(doc => {
 			renderCert(doc)
 		})
-
-		if(form.certid.value.length != 0){
-			form.remove();
-		}
+		msg.innerHTML = 'Unable to get your Data <br> Please check Certificate ID';
+		msg.style.color = 'red';
+		code.style.borderColor = 'red';
+		form.certid.value = '';
+		code.focus();
 	});
+	msg.innerHTML = 'Getting Your Data';
+	msg.style.color = '#000';
+	code.style.borderColor = '#1A73E8';
 });
