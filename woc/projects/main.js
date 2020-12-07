910007f36dfd5565aca1a9280efdec4ee2955bb9
Ankit <ankitchaudhari1598@gmail.com>
2020-12-07 15:47:15 +0530

    updated projects page

--- woc/projects/main.js
+++ woc/projects/main.js
@@ -1,11 +1,53 @@
-projects = [{
+projects = [
+{
+  desc: "Platform for building serverless applications and APIs (Node.js, React, GraphQL)",
+  forks: 299,
+  img: "https://avatars2.githubusercontent.com/u/3808429?v=4",
+  name: "webiny-js",
+  owner: "webiny",
+  stars: 3837,
+  type: "Organization",
+  url: "https://github.com/webiny/webiny-js"
+},
+{
+  desc: "Appwrite is a secure end-to-end backend server for Web, Mobile, and Flutter developers that is packaged as a set of Docker containers for easy deployment 🚀",
+  forks: 547,
+  img: "https://avatars0.githubusercontent.com/u/25003669?v=4",
+  name: "appwrite",
+  owner: "appwrite",
+  stars: 3779,
+  type: "Organization",
+  url: "https://github.com/appwrite/appwrite"
+},
+{
+  desc: "The open source Discord bot that solves all of your problems.",
+  forks: 190,
+  img: "https://avatars2.githubusercontent.com/u/39246832?v=4",
+  name: "switchblade",
+  owner: "SwitchbladeBot",
+  stars: 252,
+  type: "Organization",
+  url: "https://github.com/SwitchbladeBot/switchblade"
+},
+{
   desc: "Face Mask Detection system based on computer vision and deep learning using OpenCV and Tensorflow/Keras",
-  forks: 243,
+  forks: 270,
   img: "https://avatars2.githubusercontent.com/u/29686102?v=4",
   name: "Face-Mask-Detection",
   owner: "chandrikadeb7",
-  stars: 549,
+  stars: 592,
+  type: "User",
   url: "https://github.com/chandrikadeb7/Face-Mask-Detection"
+},
+{
+  desc: "This repo aims to contain different machine learning use cases along with the descriptions to the model architectures",
+  forks: 2,
+  img: "https://avatars3.githubusercontent.com/u/52751963?v=4",
+  name: "Grokking-Machine-Learning",
+  owner: "Abhijit2505",
+  stars: 2,
+  type: "User",
+  url: "https://github.com/Abhijit2505/Grokking-Machine-Learning"
 },
 {
   desc: "A personal portfolio application template.",
@@ -14,61 +56,38 @@ projects = [{
   name: "Portfolio-App-Template",
   owner: "Abhijit2505",
   stars: 4,
+  type: "User",
   url: "https://github.com/Abhijit2505/Portfolio-App-Template"
 },
 {
-  desc: "This repo aims to contain different machine learning use cases along with the descriptions to the model architectures",
-  forks: 0,
-  img: "https://avatars3.githubusercontent.com/u/52751963?v=4",
-  name: "Grokking-Machine-Learning",
-  owner: "Abhijit2505",
-  stars: 1,
-  url: "https://github.com/Abhijit2505/Grokking-Machine-Learning"
-},
-{
-  desc: "The open source Discord bot that solves all of your problems.",
-  forks: 188,
-  img: "https://avatars2.githubusercontent.com/u/39246832?v=4",
-  name: "switchblade",
-  owner: "SwitchbladeBot",
-  stars: 247,
-  url: "https://github.com/SwitchbladeBot/switchblade"
-},
-{
-  desc: "Appwrite is a secure end-to-end backend server for Web, Mobile, and Flutter developers that is packaged as a set of Docker containers for easy deployment 🚀",
-  forks: 545,
-  img: "https://avatars0.githubusercontent.com/u/25003669?v=4",
-  name: "appwrite",
-  owner: "appwrite",
-  stars: 3747,
-  url: "https://github.com/appwrite/appwrite"
-},
-{
-  desc: "Platform for building serverless applications and APIs (Node.js, React, GraphQL)",
-  forks: 298,
-  img: "https://avatars2.githubusercontent.com/u/3808429?v=4",
-  name: "webiny-js",
-  owner: "webiny",
-  stars: 3739,
-  url: "https://github.com/webiny/webiny-js"
-},
-{
-  desc: "MDN's Local Library with a whole lot of features (in development)",
-  forks: 2,
-  img: "https://avatars1.githubusercontent.com/u/17019944?v=4",
-  name: "express-locallibrary-tutorial",
-  owner: "Zircoz",
-  stars: 1,
-  url: "https://github.com/Zircoz/express-locallibrary-tutorial"
-},
-{
   desc: "Blogger is a basic blogging application which includes user to create an account and post their views.",
-  forks: 42,
+  forks: 59,
   img: "https://avatars3.githubusercontent.com/u/54139847?v=4",
   name: "Blogger",
   owner: "harsh-9in",
-  stars: 18,
+  stars: 24,
+  type: "User",
   url: "https://github.com/harsh-9in/Blogger"
+},
+{
+  desc: "A real-time remote service to get the Keras callbacks to the telegram including the details of metrics--&gt;pip install tensorgram",
+  forks: 2,
+  img: "https://avatars1.githubusercontent.com/u/46109386?v=4",
+  name: "TensorGram",
+  owner: "ksdkamesh99",
+  stars: 56,
+  type: "User",
+  url: "https://github.com/ksdkamesh99/TensorGram"
+},
+{
+  desc: "a simple yet powerful package manager",
+  forks: 2,
+  img: "https://avatars2.githubusercontent.com/u/72333447?v=4",
+  name: "appctl",
+  owner: "releax",
+  stars: 1,
+  type: "Organization",
+  url: "https://github.com/releax/appctl"
 },
 {
   desc: "Bringing all Quantum Algorithms under one Roof ⚡",
@@ -77,16 +96,8 @@ projects = [{
   name: "QPowerAlgo",
   owner: "QPower-Research",
   stars: 4,
+  type: "Organization",
   url: "https://github.com/QPower-Research/QPowerAlgo"
-},
-{
-  desc: "A real-time remote service to get the Keras callbacks to the telegram including the details of metrics--&gt;pip install tensorgram",
-  forks: 0,
-  img: "https://avatars1.githubusercontent.com/u/46109386?v=4",
-  name: "TensorGram",
-  owner: "ksdkamesh99",
-  stars: 14,
-  url: "https://github.com/ksdkamesh99/TensorGram"
 },
 {
   desc: "Blinker is an andorid app which will help People to Detect Colorblindness in them. It also provides recommended exercise by the Eye care experts.It will have a segment where you can read wonderful facts about our own most precious Eye.",
@@ -95,7 +106,38 @@ projects = [{
   name: "Blinker",
   owner: "wayiam",
   stars: 0,
+  type: "User",
   url: "https://github.com/wayiam/Blinker"
+},
+{
+  desc: "Stack overflow command line client. Search and browse stack overflow without leaving the terminal :computer: ",
+  forks: 204,
+  img: "https://avatars2.githubusercontent.com/u/8397274?v=4",
+  name: "socli",
+  owner: "gautamkrishnar",
+  stars: 877,
+  type: "User",
+  url: "https://github.com/gautamkrishnar/socli"
+},
+{
+  desc: "Developer Cheatsheets",
+  forks: 30,
+  img: "https://avatars1.githubusercontent.com/u/42309779?v=4",
+  name: "Cheat-Sheets",
+  owner: "VedantKhairnar",
+  stars: 22,
+  type: "User",
+  url: "https://github.com/VedantKhairnar/Cheat-Sheets"
+},
+{
+  desc: null,
+  forks: 3,
+  img: "https://avatars3.githubusercontent.com/u/66358162?v=4",
+  name: "Q-Stream",
+  owner: "Q-Stream",
+  stars: 6,
+  type: "Organization",
+  url: "https://github.com/Q-Stream/Q-Stream"
 },
 {
   desc: "Senior Year Project - AIes (object detection API)",
@@ -104,16 +146,88 @@ projects = [{
   name: "AIes_object_detection",
   owner: "Akashsindhu",
   stars: 0,
+  type: "User",
   url: "https://github.com/Akashsindhu/AIes_object_detection"
 },
 {
-  desc: "Developer Cheatsheets",
-  forks: 30,
-  img: "https://avatars1.githubusercontent.com/u/42309779?v=4",
-  name: "Cheat-Sheets",
-  owner: "VedantKhairnar",
-  stars: 19,
-  url: "https://github.com/VedantKhairnar/Cheat-Sheets"
+  desc: "This is the source code for tech_phantoms website",
+  forks: 37,
+  img: "https://avatars0.githubusercontent.com/u/61622105?v=4",
+  name: "Tech_Phantoms_website",
+  owner: "Tech-Phantoms",
+  stars: 32,
+  type: "Organization",
+  url: "https://github.com/Tech-Phantoms/Tech_Phantoms_website"
+},
+{
+  desc: "One great friend in our gadgets 💻📳📱",
+  forks: 4,
+  img: "https://avatars3.githubusercontent.com/u/53136674?v=4",
+  name: "AshTech-AI_Personal_Voice_Assistant",
+  owner: "Ash515",
+  stars: 9,
+  type: "User",
+  url: "https://github.com/Ash515/AshTech-AI_Personal_Voice_Assistant"
+},
+{
+  desc: "Scripts that will make you go WOW 😍💻 ",
+  forks: 154,
+  img: "https://avatars1.githubusercontent.com/u/47351025?v=4",
+  name: "Rotten-Scripts",
+  owner: "HarshCasper",
+  stars: 193,
+  type: "User",
+  url: "https://github.com/HarshCasper/Rotten-Scripts"
+},
+{
+  desc: "Official Website of HITK Tech Community 👩🏻‍💻👨🏻‍💻",
+  forks: 3,
+  img: "https://avatars2.githubusercontent.com/u/75066705?v=4",
+  name: "Community-Website",
+  owner: "HITK-TECH-Community",
+  stars: 2,
+  type: "Organization",
+  url: "https://github.com/HITK-TECH-Community/Community-Website"
+},
+{
+  desc: "Bringing all Data Structures and Algorithms under one Roof ⚡",
+  forks: 339,
+  img: "https://avatars1.githubusercontent.com/u/65599589?v=4",
+  name: "NeoAlgo",
+  owner: "TesseractCoding",
+  stars: 214,
+  type: "Organization",
+  url: "https://github.com/TesseractCoding/NeoAlgo"
+},
+{
+  desc: "MDN's Local Library with a whole lot of features (in development)",
+  forks: 2,
+  img: "https://avatars1.githubusercontent.com/u/17019944?v=4",
+  name: "express-locallibrary-tutorial",
+  owner: "Zircoz",
+  stars: 1,
+  type: "User",
+  url: "https://github.com/Zircoz/express-locallibrary-tutorial"
+},
+{
+  desc: "An interactive board with plugins focusing on replacing the need of multiple platforms for teaching, presenting, managing or explaining your ideas 🌎 ",
+  forks: 57,
+  img: "https://avatars0.githubusercontent.com/u/67165969?v=4",
+  name: "canvasboard",
+  owner: "Canvasbird",
+  stars: 109,
+  type: "Organization",
+  url: "https://github.com/Canvasbird/canvasboard"
+},
+{
+  desc: "Collaborative space for developers, designers and artists to share and work with their ideas.",
+  forks: 0,
+  img: "https://avatars1.githubusercontent.com/u/62296756?v=4",
+  name: "idea-lab",
+  owner: "theupweb",
+  stars: 4,
+  type: "Organization",
+  url: "https://github.com/theupweb/idea-lab"
 }]
 projects.forEach(data => {
 project_card = document.createElement('div')
@@ -132,7 +246,7 @@ project_repo_info_fork = document.createElement('div')
 
 project_card.classList.add('project-card')
 project_image.classList.add('project-image')
-project_image_link.classList.add('w-100')
+project_image_link.classList.add('h-100')
 project_body.classList.add('project-body')
 project_owner.classList.add('project-owner')
 project_tags.classList.add('project-tags')
@@ -144,7 +258,7 @@ project_repo_info_fork.classList.add('repo-info')
 
 project_image.appendChild(project_image_link)
 
-// project_card.appendChild(project_image)
+project_card.appendChild(project_image)
 project_card.appendChild(project_body)
 project_card.appendChild(project_footer)
 
@@ -161,18 +275,36 @@ project_footer_row.appendChild(project_repo_info_star)
 project_footer_row.appendChild(project_repo_info_fork)
 
 project_grid = document.querySelector('.project-grid')
+  
+  if(data.type == 'User') {
+    project_image_link.src = './img/github-user.png'
+  } else {
+    project_image_link.src = data.img
+  }
+  project_owner.innerHTML = data.author
+  project_tags = 'Tags'
+  project_link.innerHTML = data.name;
+  project_link.href = data.url;
+  // console.log(data.desc)
+  project_desc.innerHTML = data.desc;
+  project_repo_info_star.innerHTML = '<i class="fas fa-star"></i>' + data.stars
+  project_repo_info_fork.innerHTML = '<i class="fas fa-code-branch"></i>' + data.forks
+  // console.log(project_card)
+  project_grid.appendChild(project_card)
 
-	project_image_link.src = data.img
-	project_owner.innerHTML = data.author
-	project_tags = 'Tags'
-	project_link.innerHTML = data.name;
-	project_link.href = data.url;
-	console.log(data.desc)
-	project_desc.innerHTML = data.desc;
-	project_repo_info_star.innerHTML = '<i class="fas fa-star"></i>' + data.stars
-	project_repo_info_fork.innerHTML = '<i class="fas fa-code-branch"></i>' + data.forks
-	console.log(project_card)
-	project_grid.appendChild(project_card)
-
-	console.log(data)
+  console.log(data)
 })
+
+if($(window).width() < 768) {
+    $('.logo')[0].src = './img/WOC-logo.png'
+    $('.woc-badge')[0].style.opacity = '0'
+    count.forEach(ele => ele.classList.add('end'))
+}
+if($(window).width() > 768) {
+    $('.logo')[0].src = './img/logo.png'
+    $('.woc-badge')[0].style.opacity = '1'
+}
+
+$('.mobile-menu-toggle').click(function() {
+    $('nav').toggleClass('open-menu');
+});
