437d75e725423d809823800ab5bed8ad9b0ad9a2
Thejaswin <52855622+thejaswin123@users.noreply.github.com>
2021-11-13 19:59:00 +0530

    Add files via upload

--- fss/script.js
+++ fss/script.js
@@ -2,7 +2,7 @@ const hamburgermenu = document.getElementById("hamburgermenu");
         const mobileNavDropDown = document.querySelector(
           ".mobile__navbar__items"
         );
-        window.addEventListener("click", () => {
+        hamburgermenu.addEventListener("click", () => {
           if (mobileNavDropDown.classList.contains("hamburger__menu__close"))
             return mobileNavDropDown.classList.remove("hamburger__menu__close");
           return mobileNavDropDown.classList.add("hamburger__menu__close");
