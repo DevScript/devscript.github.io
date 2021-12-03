9d2a69463b7081fb98652e19ff0561c185735949
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2021-12-03 23:05:55 +0530

    faq added

--- fss/script.js
+++ fss/script.js
@@ -42,3 +42,18 @@ slider.addEventListener('mousemove', (e) => {
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
